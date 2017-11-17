988e8a20626e7bcd6cfcbb626007f665e9f0c10d
Cameron Sanchez <csanchez2237@gmail.com>
2017-11-16 21:35:54 -0700

    add del endpoint

--- db.js
+++ db.js
@@ -9,9 +9,10 @@ mongoose.connect('mongodb://localhost/messenger', { useMongoClient: true, promis
 
 const Message = mongoose.model('Message', mongoose.Schema({ from: String, message: String, to: String }));
 const create = (messageInfo) => new Message(messageInfo).save();
+const del = (query) => Message.remove(query).exec();
 
 const read = (query) => new Promise((resolve, reject) => {
   Message.find(query, (error, messages) => error ? reject(error) : resolve(messages));
 });
 
-module.exports = { create, read };
+module.exports = { create, read, del };
--- index.js
+++ index.js
@@ -6,12 +6,33 @@ const path = require('path');
 const db = require('./db');
 const app = express();
 
+const handleError = (error, res) => {
+  console.error(error);
+  res.sendStatus(500);
+};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
-app.post('/message', (req, res) => db.create(req.body));
-app.get('/message', (req, res) => db.read(req.query));
+
+app.post('/message', (req, res) => {
+  db.create(req.body)
+    .then(() => res.sendStatus(204))
+    .catch((error) => handleError(error, res));
+});
+
+app.get('/message', (req, res) => {
+  db.read(req.query)
+    .then((messages) => res.json(messages))
+    .catch((error) => handleError(error, res));
+});
+
+app.delete('/message', (req, res) => {
+  db.del(req.query)
+    .then(() => res.sendStatus(204))
+    .catch((error) => handleError(error, res));
+});
 
 app.listen(3000);
