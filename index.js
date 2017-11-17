312205a0104afc6c99c2d2b97d80be34b1cce181
Cameron Sanchez <csanchez2237@gmail.com>
2017-11-16 21:52:32 -0700

    line spacing

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const handleError = (error, res) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.post('/message', (req, res) => {
