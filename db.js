0db3a35dd61ffebc93d2c0e4163f0ab5491d0a57
Cameron Sanchez <csanchez2237@gmail.com>
2017-11-16 20:50:11 -0700

    add create and read endpoints

---
+++ db.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/messenger', { useMongoClient: true, promiseLibrary: global.Promise })
+  .then(() => console.log('connected'))
+  .catch(console.error);
+
+const Message = mongoose.model('Message', mongoose.Schema({ from: String, message: String, to: String }));
+const create = (messageInfo) => new Message(messageInfo).save();
+
+const read = (query) => new Promise((resolve, reject) => {
+  Message.find(query, (error, messages) => error ? reject(error) : resolve(messages));
+});
+
+module.exports = { create, read };
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
+const db = require('./db');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.post('/message', (req, res) => db.create(req.body));
+app.get('/message', (req, res) => db.read(req.query));
 
 app.listen(3000);
